6b637a9ae64951d41f58619f494606a23b1202dd
samluiz <samuelluizrl@outlook.com>
2022-08-26 23:05:54 -0300

    CRUD done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,65 @@ let cliente = {}
 let pedido = {}
 let price
 let totalPrice = 0
+let prop = ""
 let toast = document.getElementById('toast')
+let table = document.getElementById('table')
+let form = document.getElementById('form')
+
+// Função para deletar todos os pedidos permanentemente
+function deleteOrders() {
+  localStorage.clear()
+  closeTable()
+}
+
+// Função para ocultar pedido na lista
+function hideOrder() {
+  let td = event.target.parentNode
+  let tr = td.parentNode
+  tr.parentNode.removeChild(tr)
+}
+
+// Função que fecha a lista de pedidos ao clicar no formulário
+form.addEventListener('click', () => {
+    table.setAttribute('id', '')
+})
+
+// Função para fechar a lista de pedidos
+function closeTable() {
+  table.setAttribute('id', '')
+}
+
+// Função para abrir a lista de pedidos
+function displayTable() {
+    table.setAttribute('id', 'showTable')
+    showOrders()
+}
+
+
+// Função para mostrar os pedidos na tabela
+function showOrders() {
+  const pedidos = JSON.parse(localStorage.getItem("pedidos") || "[]")
+
+  let placeholder = document.querySelector("#data")
+  let out = ""
+  for (let pedido of pedidos) {
+    for (let i in pedido.clientes) {
+        out += `
+          <tr>
+            <td contentEditable id="idTable">${pedido.id}</td>
+            <td contentEditable>${pedido.clientes[i].name}</td>
+            <td contentEditable>${pedido.clientes[i].productName}</td>
+            <td>R$${pedido.clientes[i].price}</td>
+            <td contentEditable>R$${pedido.totalPrice}</td>
+            <td>${pedido.clientes[i].createdAt}</td>
+            <td><img src="../public/hide.svg" alt="Ocultar" class="hide" id="hide" onClick="hideOrder()" /></td>
+          </tr>
+        `     
+      }
+    }
+
+  placeholder.innerHTML = out
+}
 
 function checkout() {
   // Buscando itens do armazenamento local do navegador
@@ -99,7 +157,7 @@ function getClienteData() {
     let hour = date.getHours()
     let min = date.getMinutes()
     let sec = date.getSeconds()
-    let createdAt = hour + ":" + min + ":" + sec + " - " + dd + "/" + mm + "/" + yyyy
+    let createdAt = hour + ":" + min + ":" + sec + " " + dd + "/" + mm + "/" + yyyy
     let name = inputName.value
     let cpf = inputCpf.value
     let productName = inputProduct.value
@@ -131,8 +189,6 @@ function getClienteData() {
 
     let btn = document.getElementById('checkout')
 
-    console.log(count)
-
     if (count > 0) {
       btn.setAttribute('class', 'visible')
     }
@@ -143,8 +199,6 @@ function getClienteData() {
     toast.setAttribute('id', 'show')
     toast.innerText = "Cliente registrado! Você pode registrar mais clientes. Não esqueça de fazer o checkout quando terminar."
     toast.style.border = "1px solid green";
-    setTimeout(() => { toast.setAttribute('id', '')}, 7000)
+    setTimeout(() => { toast.setAttribute('id', '')}, 3000)
   }
-
-  console.log(clientes)
 }
